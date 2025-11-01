afb68b243447de16f72094a1e1282ad383d59f0c
Leb0wsky <youssef.soula@example.com>
2025-11-01 19:22:35 +0100

    Remove unused media files and refactor App.js for improved code readability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import logosupcom from './Assets/supcom.png';
 
 
 
-function App(){
+function App() {
 	return (
 		<div className="app" style={{
 			minHeight: '100vh',
@@ -28,11 +28,11 @@ function App(){
 			flexDirection: 'column',
 			justifyContent: 'space-between',
 		}}>
-			<header style={{display:'flex',flexDirection:'column',alignItems:'center',gap:8,margin:'32px 0 24px 0'}}>
-				<img src={logoWiempower} alt="WiEmpower Logo" style={{height:150,marginBottom:8}} />
-				<span style={{fontWeight:700,fontSize:40,letterSpacing:1,color:"rgba(82, 16, 78, 1)"}}>WiEmpower Hackathon Portal</span>
+			<header style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 8, margin: '32px 0 24px 0' }}>
+				<img src={logoWiempower} alt="WiEmpower Logo" style={{ height: 150, marginBottom: 8 }} />
+				<span style={{ fontWeight: 700, fontSize: 40, letterSpacing: 1, color: "rgba(82, 16, 78, 1)" }}>WiEmpower Hackathon Portal</span>
 			</header>
-			<div style={{flex:1,display:'flex',flexDirection:'column'}}>
+			<div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
 				<Routes>
 					<Route path="/" element={<Navigate to="/login" replace />} />
 					<Route path="/login" element={<Login />} />
@@ -40,12 +40,12 @@ function App(){
 					<Route path="/jury" element={<Jury />} />
 				</Routes>
 			</div>
-			<footer style={{display:'flex',justifyContent:'center',alignItems:'center',gap:32,padding:'15px 0 16px 0'}}>
-				<img src={logoGMC} alt="GOMYCODE Logo" style={{height:20}} />
-				<img src={logoIEEE} alt="IEEE Tunisia Section" style={{height:50}} />
-				<img src={logosupcom} alt="IEEE Tunisia Section" style={{height:50}} />
-				<img src={logoIEEE2} alt="IEEE Tunisia Section" style={{height:80}} />
-				<img src={logowie} alt="IEEE Tunisia Section" style={{height:60}} />
+			<footer style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 32, padding: '15px 0 16px 0' }}>
+				<img src={logoGMC} alt="GOMYCODE Logo" style={{ height: 20 }} />
+				<img src={logoIEEE} alt="IEEE Tunisia Section" style={{ height: 50 }} />
+				<img src={logosupcom} alt="IEEE Tunisia Section" style={{ height: 50 }} />
+				<img src={logoIEEE2} alt="IEEE Tunisia Section" style={{ height: 80 }} />
+				<img src={logowie} alt="IEEE Tunisia Section" style={{ height: 60 }} />
 			</footer>
 		</div>
 	);
